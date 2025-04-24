1a956385eaa8f7d5aac8d2d2774206b07cf7df37
Rafael <rafael_gabriel623@hotmail.com>
2025-04-23 21:27:42 -0300

    Configuração inicial da estrutura base do projeto com o pom e application configurados

---
+++ src/main/java/br/com/sol_do_amanhecer/SolDoAmanhecerApplication.java
@@ -0,0 +1,13 @@
+package br.com.sol_do_amanhecer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolDoAmanhecerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolDoAmanhecerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sol_do_amanhecer/SolDoAmanhecerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sol_do_amanhecer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolDoAmanhecerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
