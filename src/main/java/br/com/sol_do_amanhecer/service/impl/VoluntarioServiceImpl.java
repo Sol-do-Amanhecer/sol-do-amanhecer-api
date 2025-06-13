db0335617a3551f7532e552bb1e9fc2636eba787
matheussbranco <matheus.s.branco@hotmail.com>
2025-06-13 10:17:59 -0300

    Revert "Agora VoluntarioServiceImpl atualiza o status Ativo do voluntário"
    This reverts commit bf15b897970a8e06acbffe30cb6fc08d79f1e19e.

--- src/main/java/br/com/sol_do_amanhecer/service/impl/VoluntarioServiceImpl.java
+++ src/main/java/br/com/sol_do_amanhecer/service/impl/VoluntarioServiceImpl.java
@@ -76,7 +76,6 @@ public class VoluntarioServiceImpl implements VoluntarioService {
 
         voluntarioExistente.setNomeCompleto(voluntarioDTO.getNomeCompleto());
         voluntarioExistente.setDataNascimento(voluntarioDTO.getDataNascimento());
-        voluntarioExistente.setAtivo(voluntarioDTO.getAtivo());
 
         Endereco endereco = enderecoMapper.dtoParaEntity(voluntarioDTO.getEnderecoDTO());
         voluntarioExistente.setEndereco(endereco);
