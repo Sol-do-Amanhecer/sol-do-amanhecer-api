ff5c23216c0d60517d2820f9e0aee6e43050b8af
Rafael <rafael_gabriel623@hotmail.com>
2025-06-21 23:13:51 -0300

    Testando o workflow teste no swagger

--- src/main/java/br/com/sol_do_amanhecer/config/SwaggerConfig.java
+++ src/main/java/br/com/sol_do_amanhecer/config/SwaggerConfig.java
@@ -13,7 +13,7 @@ public class SwaggerConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("Api - Sol do Amanhecer - teste")
+                        .title("Api - Sol do Amanhecer")
                         .version("v1")
                         .description("Api da Ong do Sol do Amanhecer")
                         .termsOfService("https://github.com/Sol-do-Amanhecer")
